f7fdaaf2c23f6c63ae1f597a2934d3b3c245c3ab
Antonio Silva Paucar <antonios@uoreogon.eedu>
2020-10-03 08:05:18 -0700

    l

--- directions.py
+++ directions.py
@@ -1,7 +1,7 @@
 import requests
 
-lat = 44.040286
-long = -123.082158
+lat = 44.038966
+long =-123.087558
 state = "or"
 key = "bf20a5bb85774b0c9e9b7b319c92040f"
 
@@ -11,8 +11,10 @@ answer = ""
 response = requests.get(origen).json()
 address = response['StreetAddresses'][0]['StreetAddress']
 
-for i in address:
-    if i not in "0123456789":
-        answer += i
+#remove the addres number, although if the street has a number it will delete it too. Needs improvement.
+# for i in address:
+#     if i not in "0123456789":
+#         answer += i
 
-print(answer)
+
+print(address)
